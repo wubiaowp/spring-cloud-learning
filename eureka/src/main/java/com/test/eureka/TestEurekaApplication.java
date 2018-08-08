13809f60d29c69ed0a6f8aa1ba9356abb2716f31
WALKMAN <walkman@WALKMANdeMacBook-Pro.local>
2018-08-09 01:16:34 +0800

    首次提交

--- eureka/src/main/java/com/test/eureka/TestEurekaApplication.java
+++ eureka/src/main/java/com/test/eureka/TestEurekaApplication.java
@@ -1,4 +1,9 @@
 package com.test.eureka;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class TestEurekaApplication {
 }
