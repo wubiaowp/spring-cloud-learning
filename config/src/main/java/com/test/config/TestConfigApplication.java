9c01e2794444359e9959bf353386363f06069c53
walkman <wubiao_wp@163.com>
2018-08-10 00:55:23 +0800

    feat:新增eureka客户端和服务端

--- config/src/main/java/com/test/config/TestConfigApplication.java
+++ config/src/main/java/com/test/config/TestConfigApplication.java
@@ -1,10 +1,13 @@
 package com.test.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TestConfigApplication {
-
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TestConfigApplication.class).web(true).run(args);
+    }
 }
--- eureka/src/main/java/com/test/eureka/TestEurekaApplication.java
+++ eureka/src/main/java/com/test/eureka/TestEurekaApplication.java
@@ -1,9 +1,13 @@
 package com.test.eureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class TestEurekaApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TestEurekaApplication.class).web(true).run(args);
+    }
 }
