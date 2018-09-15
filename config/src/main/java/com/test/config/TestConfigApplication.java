19a8be2bd3bfea116a9dd19941388e64ee02f09d
walkman <wubiao_wp@163.com>
2018-09-15 22:39:54 +0800

    feat:新增模块

--- config/src/main/java/com/test/config/TestConfigApplication.java
+++ config/src/main/java/com/test/config/TestConfigApplication.java
@@ -3,8 +3,10 @@ package com.test.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
 public class TestConfigApplication {
     public static void main(String[] args) {
--- customer/src/main/java/com/test/customer/TestCustomerApplication.java
+++ customer/src/main/java/com/test/customer/TestCustomerApplication.java
@@ -1,4 +1,13 @@
 package com.test.customer;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
 public class TestCustomerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TestCustomerApplication.class).web(true).run(args);
+    }
+
 }
