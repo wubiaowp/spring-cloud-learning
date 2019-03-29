26c4d9c0a6197da5b21b11c2ad93a31ef7701e8a
walkman <wubiao_wp@163.com>
2019-03-30 01:51:00 +0800

    fix:新增hadoop和spark

--- zipkin/src/main/java/com/test/zipkin/ZippingApplication.java
+++ zipkin/src/main/java/com/test/zipkin/ZippingApplication.java
@@ -1,7 +1,11 @@
 package com.test.zipkin;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ZippingApplication {
@@ -9,4 +13,12 @@ public class ZippingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZippingApplication.class,args);
     }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry
+                .config()
+                .commonTags("application", applicationName);
+    }
+
 }
